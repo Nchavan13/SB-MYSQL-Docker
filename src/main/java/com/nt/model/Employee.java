00f704d09add041f5eb1d401e19fef35518f6b0c
Nilesh <nilesh.chavan6080@gmail.com>
2024-09-17 02:55:17 +0530

    updated

--- src/main/java/com/nt/model/Employee.java
+++ src/main/java/com/nt/model/Employee.java
@@ -7,24 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name="emp")
+@Table(name = "emp")
 @Data
 public class Employee implements Serializable {
-	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@SequenceGenerator(name = "gen1", sequenceName = "emp_id_seq", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(generator = "gen1", strategy = GenerationType.SEQUENCE)
 	private Integer empno;
-	
 	@Column(length = 20)
 	private String ename;
-	
 	@Column(length = 20)
 	private String job;
-	
 	private Float sal;
 }
